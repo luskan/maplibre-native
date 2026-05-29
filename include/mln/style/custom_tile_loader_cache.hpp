638011c6f68f6cd561958864c3e550ab8b469568
MarcinJ <marcinje@gmail.com>
2026-05-29 16:48:32 +0200

    Add custom tile data cache toggles

---
+++ include/mln/style/custom_tile_loader_cache.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace mln
+{
+namespace style
+{
+
+struct CustomTileLoaderDataCacheStats
+{
+    bool enabled = true;
+    uint64_t hits = 0;
+    uint64_t stores = 0;
+    uint64_t bypasses = 0;
+    std::size_t tileCount = 0;
+};
+
+void setCustomTileLoaderDataCacheEnabled(bool enabled);
+bool isCustomTileLoaderDataCacheEnabled();
+CustomTileLoaderDataCacheStats getCustomTileLoaderDataCacheStats();
+
+} // namespace style
+} // namespace mln
